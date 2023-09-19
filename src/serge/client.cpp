713e54e23cee56ffde6cbc6ab1f55d656c532a44
Sergey111Borisenko <sergey.borisenko@epitech.eu>
2023-09-19 17:16:44 +0200

    Remplace client and server with another version

--- src/serge/client.cpp
+++ src/serge/client.cpp
@@ -1,54 +1,48 @@
-// Client side implementation of UDP client-server model
-#include <bits/stdc++.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+// Client side C/C++ program to demonstrate Socket
+// programming
 #include <arpa/inet.h>
-#include <netinet/in.h>
-   
-#define PORT     4242
-#define MAXLINE 1024
-   
-// Driver code
-int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    const char *hello = "Hello from client";
-    struct sockaddr_in     servaddr;
-   
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-   
-    memset(&servaddr, 0, sizeof(servaddr));
-       
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    //servaddr.sin_addr.s_addr = INADDR_ANY;
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 
-    inet_pton(AF_INET, "10.15.193.252", &servaddr.sin_addr);
+int main(int argc, char const* argv[])
+{
+	int status, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	const char *hello = "Hello from client";
+	char buffer[1024] = { 0 };
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
 
+	// Convert IPv4 and IPv6 addresses from text to binary
+	// form
+	if (inet_pton(AF_INET, "10.15.193.252", &serv_addr.sin_addr)
+		<= 0) {
+		printf(
+			"\nInvalid address/ Address not supported \n");
+		return -1;
+	}
 
-    int n;
-    socklen_t len;
-       
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    std::cout<<"Hello message sent."<<std::endl;
-           
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    std::cout<<"Server :"<<buffer<<std::endl;
-   
-    close(sockfd);
-    return 0;
+	if ((status
+		= connect(client_fd, (struct sockaddr*)&serv_addr,
+				sizeof(serv_addr)))
+		< 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	send(client_fd, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+	valread = read(client_fd, buffer, 1024);
+	printf("%s\n", buffer);
+
+	// closing the connected socket
+	close(client_fd);
+	return 0;
 }
