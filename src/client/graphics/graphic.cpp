eed54133ef7a04f7080a88e178c1bd59ecf73ab2
Julien Vignau-Espine <julien.vignau-espine@epitech.eu>
2023-09-27 14:57:28 +0200

    Delete useless files

--- src/client/graphics/graphic.cpp
+++ src/client/graphics/graphic.cpp
@@ -1,53 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** R-type
+** File description:
+** graphic
+*/
+
 #include <SFML/Graphics.hpp>
-
-// void box_system(registry &r) {
-//     /**
-//      * @brief Setting a box system
-//      * 
-//      * @param r The registry where are stored the components
-//      * 
-//      * @return void Do not return anything
-//     */
-//     auto const &box = r.get_components<Component::box>();
-//     auto const &position = r.get_components<Component::position>();
-//     auto const &size = r.get_components<Component::size>();
-//     auto const &color = r.get_components<Component::color>();
-//     auto &window = r.get_components<Component::window>();
-//     for (size_t i = 0; i < box.size(); ++i) {
-//         if (window[i] && box[i] && position[i] && size[i] && color[i]) {
-//             box[i].x = position[i].x;
-//             box[i].y = position[i].y;
-//             box[i].width = size[i].width;
-//             box[i].height = size[i].height;
-//             box[i].color = color[i].color;
-//         }
-//     }
-// }
-
-// void text_system(registru &r) {
-//     /**
-//      * @brief Setting a text system
-//      * 
-//      * @param r The registry where are stored the components
-//      * 
-//      * @return void Do not return anything
-//     */
-//     auto const &text = r.get_components<Component::text>();
-//     auto const &position = r.get_components<Component::position>();
-//     auto const &font_size = r.get_components<Component::font_size>();
-//     auto const &color = r.get_components<Component::color>();
-//     auto const &text_content = r.get_components<Component::text_content>();
-//     auto &window = r.get_components<Component::window>();
-//     for (size_t i = 0; i < text.size(); ++i) {
-//         if (window[i] && text[i] && position[i] && color[i] && text_content[i]) {
-//             text[i].x = position[i].x;
-//             text[i].y = position[i].y;
-//             text[i].font_size = font_size[i].font_size;
-//             text[i].color = color[i].color;
-//             text[i].text_content = text_content[i].text_content;
-//         }
-//     }
-// }
 
 /**
  * @brief Setting a draw system
@@ -160,26 +118,4 @@ void logic_system(registry &r) {
             std::cout << "Velocity: " << vel.x << ", " << vel.y << std::endl;
         }
     }
-}
-
-int main() {
-    // sf::RenderWindow window(sf::VideoMode(1920, 1080), "Ma Interface Graphique");
-
-    // sf::RectangleShape box(sf::Vector2f(200, 50));
-    // box.setPosition(300, 250);
-    // box.setFillColor(sf::Color::Blue);
-
-    // while (window.isOpen()) {
-    //     sf::Event event;
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-
-    //     window.clear();
-    //     window.draw(box);
-    //     window.display();
-    // }
-
-    return 0;
 }
