f6a2ad18c6fc0e7651bcabb72c10d635fd196eec
Julien Vignau-Espine <julien.vignau-espine@epitech.eu>
2023-09-27 11:03:09 +0200

    Add text system

--- src/client/graphics/graphic.cpp
+++ src/client/graphics/graphic.cpp
@@ -10,6 +10,27 @@ void box_system(registry &r) {
         if (window[i] && box[i] && position[i] && size[i] && color[i]) {
             box[i].x = position[i].x;
             box[i].y = position[i].y;
+            box[i].width = size[i].width;
+            box[i].height = size[i].height;
+            box[i].color = color[i].color;
+        }
+    }
+}
+
+void text_system(registru &r) {
+    auto const &text = r.get_components<Component::text>();
+    auto const &position = r.get_components<Component::position>();
+    auto const &font_size = r.get_components<Component::font_size>();
+    auto const &color = r.get_components<Component::color>();
+    auto const &text_content = r.get_components<Component::text_content>();
+    auto &window = r.get_components<Component::window>();
+    for (size_t i = 0; i < text.size(); ++i) {
+        if (window[i] && text[i] && position[i] && color[i] && text_content[i]) {
+            text[i].x = position[i].x;
+            text[i].y = position[i].y;
+            text[i].font_size = font_size[i].font_size;
+            text[i].color = color[i].color;
+            text[i].text_content = text_content[i].text_content;
         }
     }
 }
