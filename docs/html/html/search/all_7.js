eebf744d95d283f05a307febef41938239594831
Viveli <maceo.jalbert@epitech.eu>
2023-10-17 13:45:23 +0200

    Change file name

--- docs/html/html/search/all_7.js
+++ docs/html/html/search/all_7.js
@@ -3,5 +3,5 @@ var searchData=
   ['health_28',['Health',['../structHealth.html',1,'']]],
   ['hitbox_29',['Hitbox',['../structHitbox.html',1,'']]],
   ['hitbox_5fsystem_30',['hitbox_system',['../classSfmlSystem.html#ade02c3f0192dbb48ca36646cf0d219cb',1,'SfmlSystem']]],
-  ['how_20to_20play_31',['How to play',['../md_How_to_play.html',1,'']]]
+  ['how_20to_20play_31',['How to play',['../md_HowToPlay.html',1,'']]]
 ];
--- docs/html/html/search/pages_2.js
+++ docs/html/html/search/pages_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['how_20to_20play_172',['How to play',['../md_How_to_play.html',1,'']]]
+  ['how_20to_20play_172',['How to play',['../md_HowToPlay.html',1,'']]]
 ];
