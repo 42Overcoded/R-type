42bd28ebda451ce1e321f5c7921980716be76e74
Sergey111Borisenko <91631204+Sergey111Borisenko@users.noreply.github.com>
2023-11-03 10:32:19 +0100

    Conditionning: Music & Sound for playing only in case of CLIENT call

--- src/gameEngine/game/gameInitialisation/initBullets.cpp
+++ src/gameEngine/game/gameInitialisation/initBullets.cpp
@@ -90,7 +90,8 @@ void gameEngine::spawn_ally_bullet(int i)
                 rect[bullet]->height = starshipJson["bullet"]["rect"][0]["height"];
                 rect[bullet]->left = starshipJson["bullet"]["rect"][0]["left"];
                 rect[bullet]->top = starshipJson["bullet"]["rect"][0]["top"];
-                sounds["soundShoot"]->play();
+                if (_type == CLIENT)
+                    sounds["soundShoot"]->play();
                 if (_health[i]->health > 30 && _health[i]->health < 85) {
                     state[bullet]->state = 1;
                     hitbox[bullet]->width = starshipJson["bullet"]["hitbox"][1]["width"];
@@ -108,7 +109,8 @@ void gameEngine::spawn_ally_bullet(int i)
                     rect[bullet]->height = starshipJson["bullet"]["rect"][2]["height"];
                     rect[bullet]->left = starshipJson["bullet"]["rect"][2]["left"];
                     rect[bullet]->top = starshipJson["bullet"]["rect"][2]["top"];
-                    sounds["soundPowerShoot"]->play();
+                    if (_type == CLIENT)
+                        sounds["soundPowerShoot"]->play();
                 }
                 _health[i]->health = 0;
                 rect[i]->width = rect[i]->baseWidth;
